4e1a047afd46144c0a6a2032b0d74a40aba3ed7b
Fuyh <fuyh_20@163.com>
2022-07-03 02:07:23 +0800

    init repo

---
+++ check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+content=$(cat ./hw.txt)
+nums=(0 0 0 0)
+i=0
+
+for line in $content
+do
+    if [ $i != 0 ]
+    then
+        nums[$i]=${line: -2}
+    fi
+    i=$((i+1))
+done
+
+if [ $[ ${nums[1]} * ${nums[2]} ] -ne ${nums[3]} ]
+then
+    exit 1
+else
+    echo "correct"
+fi
